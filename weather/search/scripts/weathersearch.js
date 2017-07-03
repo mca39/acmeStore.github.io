c583e8c81f3c6239d1ee966237e4c83677f27f49
mariah <ash15025@byui.edu>
2017-07-03 15:02:02 -0600

    updated syntax

--- weather/search/scripts/weathersearch.js
+++ weather/search/scripts/weathersearch.js
@@ -10,34 +10,32 @@ $('#query').keyup(function () {
 		$.each(data.RESULTS, function (key, val) {
 			if (val.name.search(rExp) != -1) {
 				output += '<li>';
-				output += '<a href="//www.wunderground.com' + val.l + '"title="Results for ' + val.name + '">' + val.name + '</a>';
+				output += '<a href="https//www.wunderground.com' + 'onclick="getData('+ val.lat + ', ' + val.lon + ')"' + 'title="Results for ' + val.name + '">' + val.name + '</a>';
 				output += '</li>';
 			}
-		});
+		});//end each
 		output += '</ol>';
-		$("#searchResults").html(output);
+		$("#searchResults").html(output); //send results to page
 	}); //end getJSON
 }); //end keyup
 
+
+
 // Get weather data from wunderground.com
-function getData(input) {
+function getData(lat, lon) {
 	// Get the data from the wunderground API
 	$.ajax({
-		url: "https://api.wunderground.com/api/b904d1c0aaaf88a0/geolookup/conditions/q/" +
-		input + ".json",
+		url: "https://api.wunderground.com/api/b904d1c0aaaf88a0/geolookup/conditions/q/" + lat + ', ' + lon + ".json",
 		dataType: "jsonp",
-		success: function (data) {
-			console.log(data);
-			var location = data.display_location.full;
-			var temp_f = data.current_observation.temp_f;
-			console.log('Location is: ' + location);
-			console.log('Temp is: ' + temp_f);
-			$("#cityDisplay").text(location);
-			$("title").html(location + " | Weather Center");
+		success: function (parsed_json) {
+			var location = parsed_json['location']['city'];
+			var temp_f = parsed_json['current_observation']['temp_f'];
 			$("#currentTemp").html(Math.round(temp_f) + '°');
-		 $("#summary").text(toTitleCase(data.current_observation.weather));
-			$("#high").text('High: ' + Math.round(data.forecast.forecastday.high) + '°');
-			$("#low").text('Low: ' + Math.round(data.forecast.forecastday.low) + '°');
+			$("title").html(location + " | Weather Center");
+			$("#cityDisplay").text(parsed_json.current_observation.display_location.full);
+			$("#summary").text(toTitleCase(location.current_observation.weather));
+//			$("#high").text('High: ' + Math.round(data.forecast.forecastday.high) + '°');
+//			$("#low").text('Low: ' + Math.round(data.forecast.forecastday.low) + '°');
 			$("#cover").fadeOut(250);
 		}
 	});
@@ -52,11 +50,10 @@ $("#searchResults").on("click", "a", function (evt) {
 	console.log(jsonCity);
 	index = $(this).index("a");
 	getData(returned.RESULTS[index].name);
+//	getData(index.lat, index.lon);
 	//hide search results
 	document.getElementById('searchResults').style.display = 'none';
 });
-
-
 
 // A function for changing a string to TitleCase
 function toTitleCase(str) {
